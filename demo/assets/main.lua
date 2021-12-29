0505c497d3e06e86486489b822af0c1d808d7184
MultiPain <32590972+MultiPain@users.noreply.github.com>
2021-12-29 21:37:27 +0900

    Fixed main.lua

--- demo/assets/main.lua
+++ demo/assets/main.lua
@@ -3,10 +3,7 @@ application:setBackgroundColor(0)
 
 require "SceneManager"
 require "ImGui"
-
 require "Scenes/BaseScene"
-require "Scenes/TestScene"
--- [[
 require "Scenes/Selection"
 require "Scenes/SideView"
 require "Scenes/TopDown"
@@ -15,11 +12,7 @@ Scenes = SceneManager.new{
 	["Selection"] = Selection,
 	["TopDown"] = TopDown,
 	["SideView"] = SideView,
-	["TestScene"] = TestScene,
 }
 
---Scenes:changeScene("SideView")
 Scenes:changeScene("Selection")
---Scenes:changeScene("TestScene")
 stage:addChild(Scenes)
---]]
